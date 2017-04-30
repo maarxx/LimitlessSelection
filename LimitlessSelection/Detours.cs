49df9f19a54f0fcae83bb3b80e75521d9d0f214e
maarxx <chris.lewark@gmail.com>
2017-04-30 15:35:04 -0400

    Cleanup

--- LimitlessSelection/Detours.cs
+++ LimitlessSelection/Detours.cs
@@ -14,47 +14,13 @@ namespace LimitlessSelection
     static class Detours
     {
 
-        /*
-        static MethodInfo _PlaySelectionSoundFor = typeof(WorldSelector).GetMethod("PlaySelectionSoundFor", BindingFlags.NonPublic | BindingFlags.Instance);
-        static FieldInfo _selected = typeof(WorldSelector).GetField("selected", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        [DetourMethod(typeof(WorldSelector), "Select")]
-        private static void Select(this WorldSelector self, WorldObject obj, bool playSound = true)
-        {
-            Log.Message("Hello from WorldSelector.Select!");
-            if (obj == null)
-            {
-                Log.Error("Cannot select null.");
-                return;
-            }
-            self.selectedTile = -1;
-            if (!self.IsSelected(obj))
-            {
-                if (playSound)
-                {
-                    //this.PlaySelectionSoundFor(obj);
-                    _PlaySelectionSoundFor.Invoke(self, new object[] { obj });
-                }
-                //self.selected.Add(obj);
-                _Add.Invoke(_selected, new object[] { obj });
-                WorldSelectionDrawer.Notify_Selected(obj);
-            }
-        }
-        */
-
         static MethodInfo _PlaySelectionSoundFor = typeof(Selector).GetMethod("PlaySelectionSoundFor", BindingFlags.NonPublic | BindingFlags.Instance);
         static FieldInfo _selected = typeof(Selector).GetField("selected", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        /*
-        static PropertyInfo _Count = typeof(List<object>).GetProperty("Count");
-        static MethodInfo _Add = typeof(List<object>).GetMethod("Add", BindingFlags.Instance);
-        static MethodInfo _ElementAt = typeof(List<object>).GetMethod("ElementAt", BindingFlags.Instance);
-        */
 
         [DetourMethod(typeof(Selector), "Select")]
         private static void Select(this Selector self, object obj, bool playSound = true, bool forceDesignatorDeselect = true)
         {
-            Log.Message("1");
+
             IntVec3 intVec3;
             if (obj == null)
             {
@@ -92,30 +58,15 @@ namespace LimitlessSelection
             }
             Map map = (thing == null ? ((Zone)obj).Map : thing.Map);
 
-            /*
-            Log.Message("2");
-            if (_Count == null)
-            {
-                Log.Message("_Count is null!");
-            }
-            else
-            {
-                Log.Message("COUNT: " + _Count.GetValue(_selected, null));
-            }
-            Log.Message("2.5");
-            */
             List<object> mySelected = (List<object>)_selected.GetValue(self);
 
             //for (int i = this.selected.Count - 1; i >= 0; i--)
-            //for (int i = ((int)_Count.GetValue(_selected, new object[] { }) - 1); i >= 0; i--)
             for (int i = mySelected.Count - 1; i >= 0; i--)
             {
                 //Thing item = this.selected[i] as Thing;
-                //object item = _ElementAt.Invoke(_selected, new object[] { i });
                 Thing item = mySelected[i] as Thing;
 
                 //if ((item == null ? ((Zone)this.selected[i]).Map : item.Map) != map)
-                //if ((item == null ? ((Zone)item).Map : ((Thing)item).Map) != map)
                 if ((item == null ? ((Zone)mySelected[i]).Map : item.Map) != map)
                 {
                     //self.Deselect(this.selected[i]);
@@ -129,8 +80,6 @@ namespace LimitlessSelection
                 return;
             }
             */
-
-            Log.Message("3");
 
             if (!self.IsSelected(obj))
             {
@@ -147,12 +96,10 @@ namespace LimitlessSelection
                     _PlaySelectionSoundFor.Invoke(self, new object[] { obj });
                 }
                 //this.selected.Add(obj);
-                //_Add.Invoke(_selected, new object[] { obj });
                 mySelected.Add(obj);
                 SelectionDrawer.Notify_Selected(obj);
             }
 
-            Log.Message("4");
         }
     }
 }
